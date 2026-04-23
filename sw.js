85ea20fc04bbaabc654a2bf11bfa2394350ff04a
ElFaraon65 <108295477+ElFaraon65@users.noreply.github.com>
2026-04-23 17:49:13 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Intercepta todas las peticiones de archivos .html y .json
    y las fuerza a ir al servidor real, sin caché del CDN de GitHub Pages.
 */
-const VERSION = 'asa-sw-v1';
+const VERSION = 'asa-sw-v2';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
